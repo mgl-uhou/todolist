73e2855accf02c3ae6702391c0182cee38e56d2f
mgl-uhou <miguelferreiralaurentino@gmail.com>
2025-09-08 10:08:18 -0300

    feat: add display title

--- app/src/main/java/com/mgl_uhou/todolist/ui/feature/list/ListScreen.kt
+++ app/src/main/java/com/mgl_uhou/todolist/ui/feature/list/ListScreen.kt
@@ -1,14 +1,21 @@
 package com.mgl_uhou.todolist.ui.feature.list
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mgl_uhou.todolist.domain.ToDo
@@ -34,6 +41,17 @@ fun ListContent(
     todos: List<ToDo>,
     onEvent: (ListEvent) -> Unit,
 ) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            text = "To-Do Tasks",
+            style = MaterialTheme.typography.displayMedium,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp),
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
         LazyColumn(
             modifier = Modifier,
             contentPadding = PaddingValues(16.dp)
@@ -56,6 +74,7 @@ fun ListContent(
                     Spacer(modifier = Modifier.height(16.dp))
             }
         }
+    }
 }
 
 @Preview
